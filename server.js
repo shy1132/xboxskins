188f8fbb417c3d49312aa22b0ce53ce007362431
shy <70299052+whakama@users.noreply.github.com>
2022-10-17 06:25:37 -0500

    a fucking lot
    preview downloader support
    removed extra logging
    parseua so no crashes
    title db in json
    some more comments

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const express = require('express')
 const request = require('request')
 const AdmZip = require('adm-zip')
 const { Octokit } = require('octokit')
+const tiddb = require('./files/tiddb.json')
 
 
 //setups
@@ -22,7 +23,11 @@ app.use(express.static('public'));
 app.disable('x-powered-by');
 
 //variables
-var index = {
+var skindex = {
+    'unleashx': []
+}
+
+var prindex = {
     'unleashx': []
 }
 
@@ -41,13 +46,38 @@ function parseForwarded(header){
     } else return null
 }
 
-async function updateIndexes(){
-    console.log('updating indexes')
-    index.unleashx = []
-    console.log('fetching tree sha')
+function parseUa(header){
+    if(header && header.startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1')) {
+        return true
+    } else return header
+}
+
+async function updatePreviewIndexes(){
+    console.log('updating preview indexes')
+    prindex.unleashx = []
+    //console.log('fetching tree sha')
+    var rootsha = await (await octokit.request('HEAD /repos/whakama/xbox-previews-archive/contents/')).headers.etag.split('W/"')[1].split('"')[0]
+    await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
+        owner: 'whakama',
+        repo: 'xbox-previews-archive',
+        tree_sha: rootsha
+    }).then(res=>{
+        //console.log('files fetched')
+        for (let i = 0; i < res.data.tree.length; i++) {
+            var wmv = res.data.tree[i].path.split('/').pop()
+            prindex.unleashx.push(wmv)
+        }
+        console.log('finished updating preview indexes')
+    })
+}
+
+async function updateSkinIndexes(){
+    console.log('updating skin indexes')
+    skindex.unleashx = []
+    //console.log('fetching tree sha')
     var rootsha = await (await octokit.request('HEAD /repos/whakama/xboxskins-archive/contents/')).headers.etag.split('W/"')[1].split('"')[0]
-    console.log('root sha obtained ['+rootsha+']')
-    console.log('fetching unleashx tree sha')
+    //console.log('root sha obtained ['+rootsha+']')
+    //console.log('fetching unleashx tree sha')
     var unsha;
     await octokit.request('GET /repos/whakama/xboxskins-archive/git/trees/'+rootsha).then(res=>{
         for (let i = 0; i < res.data.tree.length; i++) {
@@ -56,33 +86,36 @@ async function updateIndexes(){
             }
         }
     })
-    console.log('unleashx sha obtained ['+unsha+']')
-    console.log('fetching files')
+    //console.log('unleashx sha obtained ['+unsha+']')
+    //console.log('fetching files')
     await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
         owner: 'whakama',
         repo: 'xboxskins-archive',
         tree_sha: unsha
     }).then(res=>{
-        console.log('files fetched')
+        //console.log('files fetched')
         for (let i = 0; i < res.data.tree.length; i++) {
             var name = res.data.tree[i].path.split('/').pop()
-            index.unleashx.push({name, download: `https://raw.githubusercontent.com/whakama/xboxskins-archive/main/unleashx/${encodeURIComponent(name)}`, id: i})
+            skindex.unleashx.push({name, download: `https://raw.githubusercontent.com/whakama/xboxskins-archive/main/unleashx/${encodeURIComponent(name)}`, id: i})
         }
-        console.log('finished updating indexes')
+        console.log('finished updating skin indexes')
     })
 }
 
-updateIndexes()
-setInterval(updateIndexes, 21600000)
+updateSkinIndexes()
+updatePreviewIndexes()
+setInterval(updateSkinIndexes, 21600000)
+setInterval(updatePreviewIndexes, 21600000)
 
 
 //code
+//skin downloader
 app.get('/rss/uxdash.php', async (req, res) => { //this has nothing to do with php but the original website used php
     console.log({
-        req: 'ux_rss',
+        req: 'ux_srss',
         ip: parseForwarded(req.headers['x-forwarded-for']),
         time: Date.now(),
-        xbox: req.headers['user-agent'].startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1') ? true : req.headers['user-agent']
+        xbox: parseUa(req.headers['user-agent'])
     })
 
     var items = [
@@ -92,8 +125,8 @@ app.get('/rss/uxdash.php', async (req, res) => { //this has nothing to do with p
     ]
     var nsfwitems = []
 
-    for (let i = 0; i < index.unleashx.length; i++) {
-        var file = index.unleashx[i]
+    for (let i = 0; i < skindex.unleashx.length; i++) {
+        var file = skindex.unleashx[i]
         if(file.name.includes('[NSFW]')) {
             nsfwitems.push(uxEntry(`~${htmlEncode(file.name.slice(0, -4))}`, `http://www.xbox-skins.net/uxdash/download/${file.id}.zip`, `http://www.xbox-skins.net/uxdash/thumb/${encodeURIComponent(file.id)}.jpg`))
         } else {
@@ -109,19 +142,19 @@ app.get('/rss/uxdash.php', async (req, res) => { //this has nothing to do with p
 
 app.get('/uxdash/download/:skin.zip', async (req, res) => {
     console.log({
-        req: 'ux_dl',
+        req: 'ux_sdl',
         ip: parseForwarded(req.headers['x-forwarded-for']),
         time: Date.now(),
-        xbox: req.headers['user-agent'].startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1') ? true : req.headers['user-agent']
+        xbox: parseUa(req.headers['user-agent'])
     })
 
     if (!req.params.skin) return res.status(404).send('')
 
     const id = parseInt(req.params.skin)
-    if (id > index.unleashx.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('')
+    if (id > skindex.unleashx.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('')
 
     request({
-            url: index.unleashx[id].download,
+            url: skindex.unleashx[id].download,
             encoding: null
         },
         (err, resp, buffer) => {
@@ -136,19 +169,19 @@ app.get('/uxdash/download/:skin.zip', async (req, res) => {
 
 app.get('/uxdash/thumb/:skin.jpg', async (req, res) => {
     console.log({
-        req: 'ux_img',
+        req: 'ux_simg',
         ip: parseForwarded(req.headers['x-forwarded-for']),
         time: Date.now(),
-        xbox: req.headers['user-agent'].startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1') ? true : req.headers['user-agent']
+        xbox: parseUa(req.headers['user-agent'])
     })
 
     if (!req.params.skin) return res.status(404).send('')
 
     const id = parseInt(req.params.skin)
-    if (id > index.unleashx.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('')
+    if (id > skindex.unleashx.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('')
 
     request({
-            url: index.unleashx[id].download,
+            url: skindex.unleashx[id].download,
             encoding: null
         },
         (err, resp, buffer) => {
@@ -192,12 +225,72 @@ app.get('/uxdash/thumb/:skin.jpg', async (req, res) => {
         });
 })
 
+//preview downloader
+app.get('/games/xml/:titleid.xml', async (req, res) => {
+    //console.log(req.url)
+    console.log({
+        req: 'ux_ptid',
+        ip: parseForwarded(req.headers['x-forwarded-for']),
+        time: Date.now(),
+        xbox: parseUa(req.headers['user-agent'])
+    })
+
+    var titleid = encodeURIComponent(req.params.titleid.toUpperCase())
+    if(titleid.length != 8) return res.status(200).send(''); //200 on these because instead of requesting it to be added itll just say it doesnt exist
+    if(isNaN(parseInt(titleid, 16))) return res.status(200).send('');
+    if(!tiddb[titleid]) return res.status(200).send('');
+    var info = tiddb[titleid]
+
+    var vidid = 0
+
+    for(let i = 0; i < prindex.unleashx.length; i++) {
+        if(prindex.unleashx[i].toLowerCase().replace(/[^\w\s]/gi, '').replace(/\s+/g, ' ').trim().slice(0, -3) === info.title.toLowerCase().replace(/[^\w\s]/gi, '').replace(/\s+/g, ' ').trim()){
+            vidid = i+1
+        }
+    }
+
+    res.contentType('text/xml')
+    //res.send(`<gdbase><xbg title="${info.title}" decid="${parseInt(titleid, 16)}" hexid="${titleid}" cover="0" thumb="0" md5="" size="" liveenabled="0" systemlink="0" patchtype="0" players="0" customsoundtracks="0" genre="" esrb="" publisher="" developer="" region="0" rc="0" video="1" vc="0" vidid="${vidid}"/></gdbase>`)
+    res.send(`<gdbase><xbg title="${info.title}" decid="${parseInt(titleid, 16)}" hexid="${titleid}" video="${0-(vidid>0)}" vidid="${vidid}"/></gdbase>`)
+})
+
+app.get('/games/sendvid.php', async (req, res) => {
+    //console.log(req.url);
+    console.log({
+        req: 'ux_pvid',
+        ip: parseForwarded(req.headers['x-forwarded-for']),
+        time: Date.now(),
+        xbox: parseUa(req.headers['user-agent'])
+    })
+    
+    if(parseUa(req.headers['user-agent']) === undefined) return res.redirect(req.url)
+
+    var sid = parseInt(req.query.sid)-1
+    
+    request({
+            url: `https://raw.githubusercontent.com/whakama/xbox-previews-archive/main//${encodeURIComponent(prindex.unleashx[sid])}`,
+            encoding: null
+        },
+        (err, resp, buffer) => {
+            if (!err && resp.statusCode === 200) {
+                //console.log('sending')
+                res.send(buffer)
+                return;
+            } else {
+                //console.log('404')
+                res.status(404).send('')
+                return;
+            }
+        })
+})
+
+
+//misc
 app.get('/404/:message', (req, res) => { //unleashx will output :message due to it being the text after the last slash
     res.status(404).send('')
 })
 
-//404 handler
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => { //since its after everything it just 404s shit that isnt in this file/public dir
     res.status(404).send('')
 });
 
