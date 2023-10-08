2fa425c3c71c7586126de4fd9d724aec087f5d60
shy <70299052+whakama@users.noreply.github.com>
2023-10-07 23:09:05 -0500

    desc
    more formatting fixes
    slightly more organized
    more comments
    better logging
    micro-optimizations
    
    and other general improvements

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const path = require('path')
 const titleIds = require('./files/titleIds.json')
 
 //setups
-if(!fs.existsSync('./files/skins')) return console.log('no skins folder in ./files/skins!');
-if(!fs.existsSync('./files/previews')) return console.log('no previews folder in ./files/previews!');
+if (!fs.existsSync('./files/skins')) return console.log('no skins folder in ./files/skins!');
+if (!fs.existsSync('./files/previews')) return console.log('no previews folder in ./files/previews!');
 
 const app = express()
 
 app.set('trust proxy', true)
 app.use(express.static('public'))
-app.disable('x-powered-by')
+app.disable('x-powered-by') //disabling some headers for MAXIMUM SPEED...
+app.disable('etag') //(doesnt work in static files, but thats good because static files are for pc, anyways)
 
 //variables
 var skinIndex = []
@@ -25,24 +26,35 @@ var previewIndex = []
 var skinCache = {}
 
 //functions
+function logRequest(endpoint = 'unknown', req) {
+    var ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[req.headers['x-forwarded-for'].split(',').length - 1].trim() : 'unknown'; //if there is a forwarded for ip sent by the reverse proxy, parse it (apache usually puts a comma if you add that header manually) to only return the ip
+    var xbox = req.headers['user-agent']?.startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1'); //if the ua starts with this, it's unleashX
+    var date = new Date().toISOString()
+
+    console.log(`[${date}] ${endpoint} | ${xbox ? 'on xbox' : 'not on xbox'} | from ${ip}`)
+    return true;
+}
+
 function htmlEncode(str) {
     return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
 function rssEntry(title, link, thumb) {
-    return `<item><title>${title}</title><link>${link}</link><thumb>${thumb}</thumb></item>`;
-}
+    //validate link and if its invalid, just make it an empty string
+    try {
+        if (link) new URL(link)?.href
+    } catch {
+        link = 'http://www.xbox-skins.net/404/error'
+    }
 
-function parseForwarded(header) {
-    if (header && header.split(',')[header.split(',').length - 1]) {
-        return header.split(',')[header.split(',').length - 1].trim();
-    } else return null;
-}
+    //same but for thumbnail
+    try {
+        if (thumb) thumb = new URL(thumb)?.href
+    } catch {
+        thumb = 'http://www.xbox-skins.net/thumb.jpg'
+    }
 
-function parseUa(header) {
-    if (header && header.startsWith('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1')) {
-        return true;
-    } else return header;
+    return `<item><title>${htmlEncode(title)}</title><link>${link}</link><thumb>${thumb}</thumb></item>`;
 }
 
 async function updateSkinIndexes() {
@@ -86,7 +98,7 @@ function clearSkinCache() {
     var skinsCleared = 0
 
     var skinCacheArr = Object.keys(skinCache)
-    if(skinCacheArr.length > 10) {
+    if (skinCacheArr.length > 10) {
         skinsCleared = skinCacheArr.length
         skinCache = {}; //prevent memory leak by someone who may decide to try it
         skinCacheArr = [];
@@ -94,13 +106,13 @@ function clearSkinCache() {
 
     for (let i = 0; i < skinCacheArr.length; i++) {
         var entry = skinCache[skinCacheArr[i]]
-        if(expiryDeadline > entry.expires) {
+        if (expiryDeadline > entry.expires) {
             delete skinCache[skinCacheArr[i]];
             skinsCleared++;
         }
     }
     
-    if(skinsCleared > 0) console.log(`cleared cache for ${skinsCleared} skins`)
+    if (skinsCleared > 0) console.log(`cleared cache for ${skinsCleared} skins`)
 }
 
 updateSkinIndexes()
@@ -112,12 +124,7 @@ setInterval(clearSkinCache, 60000) //just checks every minute to see if any cach
 //code
 //skin downloader
 app.get('/rss/uxdash.php', async (req, res) => { //sends a giant xml of all the skins in the db (this has nothing to do with php but the original website used php)
-    console.log({
-        req: 'ux_srss',
-        ip: parseForwarded(req.headers['x-forwarded-for']),
-        time: Date.now(),
-        xbox: parseUa(req.headers['user-agent'])
-    })
+    logRequest('ux_rss', req)
 
     var items = [ //this will always show at the top, so i put some information here for anyone confused (since its just a giant list of skins)
         rssEntry(`! unofficial UnleashX skin downloader`, 'http://www.xbox-skins.net/404/this_is_not_a_skin', 'http://www.xbox-skins.net/thumb.jpg'),
@@ -129,14 +136,16 @@ app.get('/rss/uxdash.php', async (req, res) => { //sends a giant xml of all the 
 
     for (let i = 0; i < skinIndex.length; i++) {
         var file = skinIndex[i]
-        if (file.name.includes('[NSFW]')) {
-            nsfwItems.push(rssEntry(`~${htmlEncode(file.name.slice(0, -4))}`, `http://www.xbox-skins.net/downloads/skins/${file.id}.zip`, `http://www.xbox-skins.net/downloads/thumbs/${encodeURIComponent(file.id)}`))
+        var fileName = path.basename(file.name, path.extname(file.name))
+
+        if (fileName.includes('[NSFW]')) { //every nsfw skin has "[NSFW]" before the name
+            nsfwItems.push(rssEntry(`~${fileName}`, `http://www.xbox-skins.net/downloads/skins/${file.id}.zip`, `http://www.xbox-skins.net/downloads/thumbs/${file.id}`)) //~ before fileName to shove it down to the bottom, past all the other non nsfw skins
         } else {
-            items.push(rssEntry(`${htmlEncode(file.name.slice(0, -4))}`, `http://www.xbox-skins.net/downloads/skins/${file.id}.zip`, `http://www.xbox-skins.net/downloads/thumbs/${encodeURIComponent(file.id)}`))
+            items.push(rssEntry(fileName, `http://www.xbox-skins.net/downloads/skins/${file.id}.zip`, `http://www.xbox-skins.net/downloads/thumbs/${file.id}`))
         }
     }
 
-    items = items.concat(nsfwItems)
+    items = items.concat(nsfwItems) //just to make sure that nsfw items are indeed at the bottom (this sorts it as items, nsfwItems)
 
     var xml = `<?xml version='1.0'?><!DOCTYPE rss PUBLIC "-//Netscape Communications//DTD RSS 0.91//E" "http://my.netscape.com/publish/formats/rss-0.91.dtd"><rss version="0.91"><channel><title>www.xbox-skins.net replacement server</title><link>http://www.xbox-skins.net</link><language>en-us</language>${items.join('')}</channel></rss>`
     res.contentType('text/xml')
@@ -144,56 +153,46 @@ app.get('/rss/uxdash.php', async (req, res) => { //sends a giant xml of all the 
 })
 
 app.get('/downloads/skins/:skin.zip', async (req, res) => { //sends the zip file for a skin by it's id (obtained from the index of the skin in ux_srss)
-    console.log({
-        req: 'ux_sdl',
-        ip: parseForwarded(req.headers['x-forwarded-for']),
-        time: Date.now(),
-        xbox: parseUa(req.headers['user-agent'])
-    })
+    logRequest('ux_skin', req)
 
     if (!req.params.skin) return res.status(404).send('');
 
     const id = parseInt(req.params.skin)
     if (id > skinIndex.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('');
     
-    if(skinCache[id]?.zip) {
-        skinCache[id] = { ...skinCache[id], expires: Date.now()+300000 }
+    if (skinCache[id]?.zip) {
+        skinCache[id] = { ...skinCache[id], expires: Date.now() + 300000 }
         res.contentType('application/zip')
         return res.send(skinCache[id].zip);
     }
 
     var buffer = await fs.promises.readFile(skinIndex[id].path)
-    skinCache[id] = { ...skinCache[id], zip: buffer, expires: Date.now()+300000 }
+    skinCache[id] = { ...skinCache[id], zip: buffer, expires: Date.now() + 300000 }
     res.contentType('application/zip')
     res.send(buffer)
 })
 
 app.get('/downloads/thumbs/:skin', async (req, res) => { //sends a thumbnail of the skin, skins are supposed to have a file in them for this so it reads through the files and tries to find it
-    console.log({
-        req: 'ux_simg',
-        ip: parseForwarded(req.headers['x-forwarded-for']),
-        time: Date.now(),
-        xbox: parseUa(req.headers['user-agent'])
-    })
+    logRequest('ux_thumb', req)
 
     if (!req.params.skin) return res.status(404).send('');
 
     const id = parseInt(req.params.skin)
     if (id > skinIndex.length - 1 || id < 0 || isNaN(id)) return res.status(404).send('');
 
-    if(skinCache[id]?.thumbnailFile) {
-        skinCache[id] = { ...skinCache[id], expires: Date.now()+300000 }
-        if(skinCache[id].thumbnailFile === 'no_thumbnail') return res.status(200).send('');
+    if (skinCache[id]?.thumbnailFile) {
+        skinCache[id] = { ...skinCache[id], expires: Date.now() + 300000 }
+        if (skinCache[id].thumbnailFile === 'no_thumbnail') return res.status(200).send('');
         res.contentType(skinCache[id].thumbnailMimeType)
         return res.send(skinCache[id].thumbnailFile);
     }
 
-    if(skinCache[id]?.zip) {
-        skinCache[id] = { ...skinCache[id], expires: Date.now()+300000 }
+    if (skinCache[id]?.zip) {
+        skinCache[id] = { ...skinCache[id], expires: Date.now() + 300000 }
         var zip = new AdmZip(skinCache[id].zip)
     } else {
         var buffer = await fs.promises.readFile(skinIndex[id].path)
-        skinCache[id] = { ...skinCache[id], zip: buffer, expires: Date.now()+300000 }
+        skinCache[id] = { ...skinCache[id], zip: buffer, expires: Date.now() + 300000 }
         var zip = new AdmZip(buffer)
     }
 
@@ -208,7 +207,7 @@ app.get('/downloads/thumbs/:skin', async (req, res) => { //sends a thumbnail of 
 
         if ((entryName.startsWith('preview') || entryName.startsWith('screenshot')) && validFormats.includes(entryExtension)) {
             let thumbnailBuffer = entry.getData()
-            skinCache[id] = { ...skinCache[id], thumbnailFile: thumbnailBuffer, thumbnailMimeType: 'image/' + entryExtension, expires: Date.now()+300000 }
+            skinCache[id] = { ...skinCache[id], thumbnailFile: thumbnailBuffer, thumbnailMimeType: 'image/' + entryExtension, expires: Date.now() + 300000 }
             res.contentType('image/' + entryExtension)
             res.send(entry.getData())
             break;
@@ -224,7 +223,7 @@ app.get('/downloads/thumbs/:skin', async (req, res) => { //sends a thumbnail of 
             res.send(contenders[0].getData())
             break;
         } else if (i >= Object.values(zipEntries).length - 1) {
-            skinCache[id] = { ...skinCache[id], thumbnailFile: 'no_thumbnail', thumbnailMimeType: 'no_thumbnail', expires: Date.now()+300000 }
+            skinCache[id] = { ...skinCache[id], thumbnailFile: 'no_thumbnail', thumbnailMimeType: 'no_thumbnail', expires: Date.now() + 300000 }
             res.status(200).send('')
             break;
         }
@@ -233,12 +232,7 @@ app.get('/downloads/thumbs/:skin', async (req, res) => { //sends a thumbnail of 
 
 //preview downloader
 app.get('/games/xml/:titleId.xml', async (req, res) => { //sends an xml file from a game's title id
-    console.log({
-        req: 'ux_ptid',
-        ip: parseForwarded(req.headers['x-forwarded-for']),
-        time: Date.now(),
-        xbox: parseUa(req.headers['user-agent'])
-    })
+    logRequest('ux_game', req)
 
     var titleId = encodeURIComponent(req.params.titleId.toUpperCase())
     if (titleId.length != 8) return res.status(200).send(''); //200 on these because instead of requesting it to be added (not a feature never will be) itll just say it doesnt exist
@@ -254,19 +248,14 @@ app.get('/games/xml/:titleId.xml', async (req, res) => { //sends an xml file fro
     }
 
     res.contentType('text/xml')
-    //res.send(`<gdbase><xbg title="${info.title}" decid="${parseInt(titleid, 16)}" hexid="${titleid}" cover="0" thumb="0" md5="" size="" liveenabled="0" systemlink="0" patchtype="0" players="0" customsoundtracks="0" genre="" esrb="" publisher="" developer="" region="0" rc="0" video="1" vc="0" vidid="${vidid}"/></gdbase>`)
     res.send(`<gdbase><xbg title="${htmlEncode(info.title)}" decid="${parseInt(titleId, 16)}" hexid="${titleId}" video="${0-(videoId>0)}" vidid="${videoId}"/></gdbase>`)
+    //res.send(`<gdbase><xbg title="${info.title}" decid="${parseInt(titleid, 16)}" hexid="${titleid}" cover="0" thumb="0" md5="" size="" liveenabled="0" systemlink="0" patchtype="0" players="0" customsoundtracks="0" genre="" esrb="" publisher="" developer="" region="0" rc="0" video="1" vc="0" vidid="${vidid}"/></gdbase>`)
 })
 
 app.get('/games/sendvid.php', async (req, res) => { //sends a preview of a game by it's id
-    console.log({
-        req: 'ux_pvid',
-        ip: parseForwarded(req.headers['x-forwarded-for']),
-        time: Date.now(),
-        xbox: parseUa(req.headers['user-agent'])
-    })
+    logRequest('ux_vid', req)
 
-    if (parseUa(req.headers['user-agent']) === undefined) return res.redirect(req.url); //weird unleashx bug
+    if (!req.headers['user-agent']) return res.redirect(req.url); //weird unleashx bug
 
     var videoId = parseInt(req.query.sid) - 1
     if (isNaN(videoId)) return res.status(404).send('');
@@ -277,7 +266,7 @@ app.get('/games/sendvid.php', async (req, res) => { //sends a preview of a game 
 })
 
 //misc
-app.get('/404/:message', (req, res) => { //unleashx will output :message due to it being the text after the last slash
+app.get('/404/:message', (req, res) => { //unleashX will output :message due to it being the text after the last slash
     res.status(404).send('')
 })
 
@@ -285,7 +274,7 @@ app.all('*', (req, res) => { //since its after everything it just 404s shit that
     res.status(404).send('')
 })
 
-app.use((error, req, res, next) => { //error handler so i dont accidentally leak anything/mess with an xbox in a weird way lol
+app.use((error, req, res, next) => { //error handler so i dont accidentally leak anything somehow/mess with unleashX in a weird way lol
     console.log(`error on ${req.path}: ${error.message}`)
     res.status(500).send('')
 })
